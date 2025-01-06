f3d6cc83ded280bf74473a8811c758519dfc8619
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-06 23:57:34 +0100

    Update BlockchainApplication.java

--- BlockchainApplication.java
+++ BlockchainApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.vit.isf.jcomponent.blockchain")
+@ComponentScan("com.hehe.buuu.jcomponent.blockchain")
 public class BlockchainApplication {
 
 	public static void main(String[] args) {
